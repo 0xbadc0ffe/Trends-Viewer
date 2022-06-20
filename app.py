b16e3b7e1312c9845d343eafa40c812f1a135d99
0xbadc0ffe <m.viscione98@gmail.com>
2022-06-20 21:21:31 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,390 @@
+import yfinance as yf
+from pytrends.request import TrendReq
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import correlation as corr
+from PIL import Image
+
+#image = Image.open('img.png')
+#st.image(image, use_column_width=True)
+
+st.write("""
+# ★ Stock Price - Google Trends correlations 
+
+Shown area the stock *opening price* and ***volume*** of the given ticker
+
+""")
+
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+# define the ticker symbol
+tickerSymbol_default = 'AAPL' #'GOOGL'
+tickerSymbol = st.text_input("Ticker Symbol", tickerSymbol_default)
+#tickerSymbol = tickerSymbol.strip()
+
+start_date = '2021-04-25' #'2010-5-31'
+start_date = st.text_input("Starting date  [yyyy-mm-dd]", start_date)
+
+end_date = '2022-04-19' #'2020-5-31'
+end_date = st.text_input("Ending date  [yyyy-mm-dd]", end_date)
+
+period = '1d'
+period = st.text_input("Period", period)
+
+#get data on this ticker
+stock_Data = yf.Ticker(tickerSymbol)
+# get the hystorical price for this ticker
+
+stock_df = stock_Data.history(period=period, start=start_date, end=end_date)
+# Open  High    Low Close   Volume  Dividends   Stock Splits
+
+
+see_volume = st.checkbox("See Volume", value=False)
+flat_stocks = st.checkbox("Flat Stocks")
+normalize = st.checkbox("Normalize all", value=True)
+polish_all = st.checkbox("Polish all", value=True)
+if polish_all:
+    polish_n = int(st.text_input("Polish n:", "10"))
+st.write("----------")
+st.write("**Correlations settings**")
+max_shift = int(st.text_input("Max trends day-shift allowed:", "20"))
+rtrends_numb = int(st.text_input("RTrends number:", "10"))
+
+
+if stock_df.empty:
+    st.write("**This ticker do not exists!**")
+else:
+
+    ####### STREND
+    if see_volume:
+        st.header("★ Volume and Stock Opening price")
+        st.line_chart(stock_df.Volume)
+    else:
+        st.header("★ Stock Opening price")
+    st.line_chart(stock_df.Open)
+    #st.line_chart(stock_df.Open-stock_df.Close)
+    
+
+    min_open = np.min(stock_df["Open"])
+    max_open = np.max(stock_df["Open"])
+    init_stock_value = stock_df["Open"][0]
+
+    st.write(f"""
+    **Min open**:    {np.round(min_open,3)}
+    
+    **Max open**:    {np.round(max_open,3)}
+    """)
+
+    dates = corr.compute_dates_vec(stock_df.index)
+
+    strend_pday = corr.fill_trend(dates, stock_df["Open"])
+    if flat_stocks:
+        strend_pday = corr.flatter(strend_pday)
+
+    tmp = [i for i in range(dates[0], len(strend_pday)+dates[0])]
+    dates_fill = []
+    for d in tmp: dates_fill.append(corr.to_date(start_date, d))
+
+    strend_pday = pd.DataFrame(
+        strend_pday,
+        columns=["Open"]
+    )
+
+    if polish_all:
+        strend_pday["Unpolished ST Data"] = strend_pday.Open
+    strend_pday.index = dates_fill
+    
+    st.subheader("Worked Stock Trend")
+    if flat_stocks:
+        min_open_flat = min(strend_pday.Open)
+        max_open_flat = max(strend_pday.Open)
+        if normalize:
+            strend_pday = (strend_pday-min_open_flat)/(max_open_flat-min_open_flat)
+
+
+    else:
+        if normalize:
+            strend_pday = (strend_pday-min_open)/(max_open-min_open) 
+
+
+    if polish_all:
+        strend_pday.Open = corr.polish(strend_pday.Open, polish_n)
+    
+    st.line_chart(strend_pday)
+
+    if flat_stocks:
+        st.write(f"""
+            **Min open flat**:    {np.round(min_open_flat,3)}
+            
+            **Max open flat**:    {np.round(max_open_flat,3)}
+            """)
+
+
+
+    ####### GTREND
+
+    st.header("★ Google Trend")
+    pytrend = TrendReq()
+    std_list = start_date.split("-")
+    edd_list = end_date.split("-")
+    trend_str = st.text_input("Google Trends query:", "apple")
+    geo_tag = st.text_input("""Geographic location [e.g. None (global), GB, GB-ENG, US]:""", "GB") #"GB-ENG" #US
+    gprop = st.text_input("""GT property [e.g. None (all channels), news, images, youtube or froogle]:""", "") #"news"
+
+
+    empowered_gt = st.checkbox("empowered historical interest", value=False)
+    if empowered_gt:
+        gtrend_df = pytrend.get_historical_interest([trend_str],
+                year_start=int(std_list[0]), month_start=int(std_list[1]), day_start=int(std_list[2]),
+                year_end=int(edd_list[0]), month_end=int(edd_list[1]), day_end=int(edd_list[2]),
+                frequency="daily", geo=geo_tag, gprop=gprop
+            ) 
+    else:
+    
+        pytrend.build_payload([trend_str], cat=0, timeframe=start_date+" "+end_date, geo=geo_tag, gprop=gprop)
+        gtrend_df = pytrend.interest_over_time()
+
+    if st.checkbox("see tabular data", value=False):
+        gtrend_df
+
+    if st.checkbox("compare with another", value=False):
+        trend_str2 = st.text_input("Google Trends query:", "iphone")
+        geo_tag2 = st.text_input("""Geographic location:""", "") #"GB-ENG" #US
+        gprop2 = st.text_input("""GT property:""", "news") #"news"
+        pytrend.build_payload([trend_str2], cat=0, timeframe=start_date+" "+end_date, geo=geo_tag2, gprop=gprop2)
+        gtrend_df2 = pytrend.interest_over_time()
+        st.line_chart(pd.concat([gtrend_df[trend_str], gtrend_df2[trend_str2]], axis=1))
+    else:
+        st.line_chart(gtrend_df[trend_str])
+
+
+
+    min_gtrend = np.min(gtrend_df[trend_str])
+    max_gtrend = np.max(gtrend_df[trend_str])
+    st.write(f"""
+    **GTrend Min**: {min_gtrend}
+    
+    **GTrend Max**: {max_gtrend}
+    """)
+
+    st.subheader("Worked Google Trend")
+    if normalize:   
+        gtrend_df[trend_str] = (gtrend_df[trend_str]-min_gtrend)/(max_gtrend-min_gtrend)
+    
+    if empowered_gt:
+        gtrend_pday = pd.DataFrame(
+            gtrend_df[trend_str],
+            columns=[trend_str]
+        )
+    else:
+        gtrend_pday = []
+        rep = 7  #ogni dato del file csv trend viene ripetuto rep volte
+        for p in gtrend_df[trend_str]:
+            for i in range(rep):
+                gtrend_pday.append(p)
+
+        tmp = [i for i in range(dates[0], len(gtrend_pday)+dates[0])]
+        dates_fill = []
+        for d in tmp: dates_fill.append(corr.to_date(start_date, d))
+        
+        gtrend_pday = pd.DataFrame(
+            gtrend_pday,
+            columns=[trend_str]
+        )
+        gtrend_pday.index = dates_fill
+
+    if polish_all:
+        gtrend_pday["Unpolished GT Data"] = gtrend_pday[trend_str]
+        gtrend_pday[trend_str] = corr.polish(gtrend_pday[trend_str], polish_n)
+
+
+    st.line_chart(gtrend_pday)
+    show_plot = st.checkbox("Show both Google and Stock trends", value=True)
+    if show_plot:
+        st.subheader("Google Trend vs Stock Open prices")
+        if st.checkbox("Show unpolished", value=True):
+            st.line_chart(pd.concat([strend_pday.Open, strend_pday["Unpolished ST Data"], gtrend_pday[trend_str], gtrend_pday["Unpolished GT Data"]], axis=1))
+        else:
+            st.line_chart(pd.concat([strend_pday.Open, gtrend_pday[trend_str]], axis=1))
+
+
+    ####### RANDOM WALK
+    st.header("★ Random Walk")
+    rndWalk = strend_pday.copy()[[]] #copy stock_df and erase all columns
+    max_length = int(st.text_input("max consecutive steps:", "1"))
+    st.button('refresh')
+    rndWalk["random walk"] = corr.brownian(init_stock_value, len(strend_pday.Open), max_length=max_length)
+    min_open_rnd = min(rndWalk["random walk"])
+    max_open_rnd = max(rndWalk["random walk"])
+    if normalize:
+            rndWalk["random walk"] = (rndWalk["random walk"]-min_open_rnd)/(max_open_rnd-min_open_rnd)  
+
+    if polish_all:
+        rndWalk["Unpolished RND Data"] = rndWalk["random walk"]
+        rndWalk["random walk"] = corr.polish(rndWalk["random walk"], polish_n)
+
+    st.line_chart(rndWalk)
+
+    show_plot = st.checkbox("Show both Random Walk and Stock trends", value=True)
+    if show_plot:
+        st.subheader("Random Walk vs Stock Open prices")
+        st.line_chart(pd.concat([strend_pday.Open, strend_pday["Unpolished ST Data"], rndWalk["random walk"], rndWalk["Unpolished RND Data"]], axis=1))
+
+
+
+    
+    ####### Correlation G-S Trends
+    st.header("★ G-S Trends Correlation")
+    corr_g = []
+    mcorr_g = []
+    lk_g = []
+    st.write("--------------")
+    st.write(f'**Shift di x giorni:$~~~~~~~~~~$correlation , likelihood | magnitude-correlation**')
+    
+    for i in range (max_shift):
+        r, lk = corr.correlation(strend_pday.Open, gtrend_pday[trend_str], i)
+        mang_cor = corr.correlation_Magnitude(strend_pday.Open, gtrend_pday[trend_str], i)
+        corr_g.append(r)
+        mcorr_g.append(mang_cor)
+        lk_g.append(lk)
+        st.write(f'Shift di {i} giorni:$~~~~~~~~~~${np.round(r,3)} ,   {np.round(lk,3)} | {np.round(mang_cor,3)}')
+    
+    maxmin_stat_g = corr.maxmin_shift(strend_pday.Open, gtrend_pday[trend_str], max_shift=max_shift, corr_type="classic")
+    maxmin_stat_mag_g = corr.maxmin_shift(strend_pday.Open, gtrend_pday[trend_str], max_shift=max_shift, corr_type="magnitude")
+    st.write()
+    st.write(f"""
+    --------------
+
+    **Max correlation**: {np.round(maxmin_stat_g[1],3)} [{maxmin_stat_g[0]} days shift]
+
+    **Min correlation**: {np.round(maxmin_stat_g[3],3)} [{maxmin_stat_g[2]} days shift]
+    """)
+
+    st.write(f"""
+    --------------
+
+    **Max magnitude-correlation**: {np.round(maxmin_stat_mag_g[1],3)} [{maxmin_stat_mag_g[0]} days shift]
+
+    **Min magnitude-correlation**: {np.round(maxmin_stat_mag_g[3],3)} [{maxmin_stat_mag_g[2]} days shift]
+
+    --------------
+    """)
+
+    corr_g_df = pd.DataFrame(
+    )
+    corr_g_df["correlation"] = corr_g
+    corr_g_df["likelihood"] = lk_g
+    corr_g_df["magnitude-correlation"] = mcorr_g
+
+
+    st.subheader("Magnitude Correlation")
+    st.line_chart(corr_g_df["magnitude-correlation"])
+
+
+    # corr_g_vec, lk_g_vec = shifted_coorelation(strend_pday, gtrend_pday, max_shift=max_shift, corr_type="classic"))
+    # plt.figure()
+    # plt.plot(corr_g_vec)
+    # plt.figure()
+    # plt.plot(lk_g_vec)
+
+
+    ####### Correlation S-R Trends
+    st.header("★ S-R Trends Correlation")
+    corr_r = []
+    lk_r = []
+    mcorr_r = []
+    stdv = 0
+    st.write("--------------")
+    st.write(f'**Shift di x giorni:$~~~~~~~~~~$correlation , likelihood | magnitude-correlation**')
+    for i in range (max_shift):
+        r, lk, inc_stdv = corr.correlation(strend_pday.Open, rndWalk["random walk"], i, return_std=True)
+        stdv += inc_stdv
+        corr_r.append(r)
+        lk_r.append(lk)
+        mang_cor = corr.correlation_Magnitude(strend_pday.Open, rndWalk["random walk"], i)
+        mcorr_r.append(mang_cor)
+        st.write(f'Shift di {i} giorni:$~~~~~~~~~~${np.round(r,3)} ,   {np.round(lk,3)} | {np.round(mang_cor,3)}')
+
+        
+    maxmin_stat_r = corr.maxmin_shift(strend_pday.Open, rndWalk["random walk"], max_shift=max_shift, corr_type="classic")
+    maxmin_stat_mag_r = corr.maxmin_shift(strend_pday.Open, rndWalk["random walk"], max_shift=max_shift, corr_type="magnitude")
+    st.write()
+    st.write(f"""
+    --------------
+
+    **Max correlation**: {np.round(maxmin_stat_r[1],3)} [{maxmin_stat_r[0]} days shift]
+
+    **Min correlation**: {np.round(maxmin_stat_r[3],3)} [{maxmin_stat_r[2]} days shift]
+    """)
+
+    st.write(f"""
+    --------------
+
+    **Max magnitude-correlation**: {np.round(maxmin_stat_mag_r[1],3)} [{maxmin_stat_mag_r[0]} days shift]
+
+    **Min magnitude-correlation**: {np.round(maxmin_stat_mag_r[3],3)} [{maxmin_stat_mag_r[2]} days shift]
+
+    --------------
+    """)
+
+    ####### Log
+    st.header("★ Correlations Log")
+
+    for i in range(rtrends_numb-1):
+        #prv_rand = random_walk
+        random_walk = corr.brownian(init_stock_value, len(strend_pday.Open), 0.5, max_length=max_length)
+        if normalize:     
+            min_rnd = np.min(random_walk)
+            max_rnd = np.max(random_walk)
+            random_walk = (random_walk -min_rnd)/(max_rnd-min_rnd)
+
+        if polish_all:
+            random_walk = corr.polish(random_walk, polish_n)
+
+        for i in range (max_shift):
+            #r, lk = correlation_noMagnitude(prv_rand, random_walk, i) 
+            r, lk, inc_stdv = corr.correlation(strend_pday.Open, random_walk, i, return_std=True)
+            mang_cor = corr.correlation_Magnitude(strend_pday.Open, random_walk, i)
+            mcorr_r.append(mang_cor)
+            stdv += inc_stdv
+            corr_r.append(r)
+            lk_r.append(lk)
+
+    rtrends_numb = len(lk_r)
+    stdv = stdv/rtrends_numb
+
+
+
+    # Statistics Log
+
+    st.write("""--------------""")
+    st.write(f"""Mean correlation with GTrend:$~~~~~~~~~~${np.round(np.mean(corr_g),3)}""") 
+    st.write(f"""Mean likely with GTrend:$~~~~~~~~~~~~~~~~${np.round(np.mean(lk_g),3)}""")
+    st.write(f"""Mean magn correl with GTrend:$~~~~~~~~~~${np.round(np.mean(mcorr_g),3)}""")
+    st.write(f"Max correlation with GTrend:$~~~~~~~~~~${np.round(maxmin_stat_g[1],3)}     [shift: {maxmin_stat_g[0]}]")
+    st.write(f"Min correlation with GTrend:$~~~~~~~~~~${np.round(maxmin_stat_g[3],3)}     [shift: {maxmin_stat_g[2]}]")
+    st.write("""--------------""")
+
+    st.write(f"""Mean correlation with RndWlk:$~~~~~~~~~~${np.round(np.mean(corr_r),3)}""") 
+    st.write(f"""Mean likely with RndWlk:$~~~~~~~~~~~~~~~~${np.round(np.mean(lk_r),3)}""")
+    st.write(f"""Mean magn correl with RTrend:$~~~~~~~~~~${np.round(np.mean(mcorr_r),3)}""")
+    st.write(f"Max correl with first RTrend:$~~~~~~~~~~${np.round(maxmin_stat_r[1],3)}     [shift: {maxmin_stat_r[0]}]")
+    st.write(f"Min correl with first RTrend:$~~~~~~~~~~${np.round(maxmin_stat_r[3],3)}     [shift: {maxmin_stat_r[2]}]\n")
+    st.write("""--------------""")
+
+
+    st.subheader("Confidence from likelihood calculator")
+    lk_target = float(st.text_input("reference likelihood:", "1.645"))
+    # p =  0.05, 0.025,  0.01, 0.005, 0.001, 0.0005 for
+    # z = 1.645, 1.960, 2.327, 2.576, 3.091,  3.291
+    st.write(f"Total Random trends:$~~~~~~~~~~~~~~~~~~~~${rtrends_numb}")
+    st.write(f"Standard Deviation RTrends:$~~~~~~~~~~${np.round(stdv,5)}")
+    st.write(f"Expected probability for {lk_target}:$~~~~~~~${np.round(corr.conf_int_prob(lk_target),4)}")
+    lkstdv = np.round(lk_target*stdv,4)
+    st.write(f"Likelihood*stdv:$~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${lkstdv}")
+    st.write(f"Prob(correlation > 0.5 + {lkstdv}):$~~~~~~~~~~~${np.sum(np.array(lk_r)>lk_target)/rtrends_numb}")
+    st.write(f"Prob(correlation < 0.5 - {lkstdv}):$~~~~~~~~~~~${np.sum(np.array(lk_r)<-lk_target)/rtrends_numb}")
+    st.write(f"Prob(|correlation-0.5| > {lkstdv}):$~~~~~~~~~~~${(np.sum(np.array(lk_r)>lk_target) + np.sum(np.array(lk_r)<-lk_target))/rtrends_numb}")
+    
---
+++ correlation.py
@@ -0,0 +1,335 @@
+from fileinput import filename
+from logging import raiseExceptions
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import math
+
+
+
+def to_date(start_date, day_of_year):
+    
+    cnt = 0
+    init_d = int(start_date[:4])
+    p = 0
+    while True:
+        try:
+            d = pd.to_datetime(str(init_d + cnt) + str(day_of_year - p), format='%Y%j')
+            break
+        except:
+            cnt += 1
+            # leap years
+            if (init_d+cnt)%4 == 0:
+                p += 366
+            else:
+                p += 365 
+    return d
+
+
+def fill_trend(x_axis, trend, line=True):
+    out = []
+    prev = 0
+    x0 = x_axis[0]
+    cnt = 0
+    skip = 0
+    for i in range(x0, x_axis[-1]+1):
+        if skip>0:
+            skip -=1
+            continue
+        if i in x_axis:
+            out.append(trend[cnt])
+            prev = trend[cnt]
+            cnt += 1
+        else:
+            if not line: 
+                if i+1 in x_axis:
+                    out.append((prev+trend[cnt+1])/2)
+                else:
+                    out.append(prev)
+            else:
+                for j in range(i+1, x_axis[-1]+1):
+                    if j in x_axis:
+                        for k in range(i, j):
+                            out.append((1-(k-i+1)/(j-i+1))*prev + (k-i+1)/(j-i+1)*trend[cnt])
+                        skip = j-i-1
+                        break
+                    elif j == x_axis[-1]+1:
+                        for k in range(i, j):
+                            out.append(prev)
+                        return out
+    return out
+
+
+def brownian(x0, steps, alpha=0.5, p=0.5, max_length=5):
+
+    res = []
+    x = x0
+    while len(res)<steps:
+        n = np.random.randint(1,max_length+1)
+        spin = 2*int(np.random.random()>p) -1
+        res += [x+i*spin*alpha for i in range(1,n+1)]
+        x += spin*n*alpha
+    return res[:steps]
+
+
+def trend_derivative(trend_pday, eps=1):
+    d_trend = []
+    t_prev = None
+    for t in trend_pday:
+        if t_prev is None:
+            d_trend.append(0)
+            t_prev = t
+        else:
+            d_trend.append((t-t_prev)/eps)
+            t_prev = t
+    return d_trend
+
+
+def flatter(trend):
+    a = np.arange(len(trend))
+    b = a.reshape(len(trend),1)    
+    
+    alpha = np.linalg.lstsq(b, trend, rcond=None)[0]
+    
+    for i in range(len(trend)):
+        trend[i] = trend[i] - alpha*i    
+    return (trend)
+ 
+## Polish rende più smooth la serie trend che gli viene passata come argomento eseguendo la media pesata tra un punto e i suoi 
+## vicini n volte  
+
+def polish(trend, n):
+
+    for k in range(n):
+        i = 0
+        for i in range(len(trend)):
+            
+            
+            if i==0:
+                trend[i] = 0.75*trend[i] + 0.25*trend[i+1]
+                #print ('i==0')
+            else: 
+                if i==(len(trend)-1): 
+                    trend[i] = 0.25*trend[i-1] + 0.5*trend[i]
+                #    print('i==365')
+                else:
+                    trend[i] = 0.25*trend[i-1] + 0.5*trend[i] + 0.25*trend[i+1]
+        i=0
+    return(trend)
+            
+
+def correlation (strend, gtrend, shift, return_std=False):
+
+    series_strend = []
+    series_gtrend = []
+    
+    ## Converto le serie originali in serie con valori {0, 1, -1} ove 0 significa nessuno incremento, 1 incremento rispetto al 
+    ## valore precedente, -1 decremento rispetto al valore precedente
+    
+    for i in range(len(strend)):
+        if i < (len(strend)-1):
+            if strend[i] <= strend[i+1]:
+                if strend[i] < strend[i+1]: 
+                    series_strend.append(1)
+                else:
+                    series_strend.append(0)
+            else:
+                series_strend.append(-1)
+ 
+ 
+    for i in range(len(gtrend)):
+        if i < (len(gtrend)-1):
+            if gtrend[i] <= gtrend[i+1]:
+                if gtrend[i] < gtrend[i+1]: 
+                    series_gtrend.append(1)
+                else:
+                    series_gtrend.append(0)
+            else:
+                series_gtrend.append(-1)    
+    
+    count = 0
+    for i in range(min(len(series_strend), len(series_gtrend)) - shift):
+        if series_strend[i+shift] == series_gtrend[i]:
+            count += 1
+        
+    r = count / (min(len(series_strend), len(series_gtrend)) - shift)  
+    
+
+    n = min(len(series_strend), len(series_gtrend)) - shift - 1
+    stdv = (0.25/n)**(1/2)  #standard deviation di una distribuzione binomiale = (1/n*p*(1-p))^(1/2) con p = 0.5
+
+    
+    likely = (r-0.5)/stdv
+    if return_std:
+        return(r, likely, stdv)
+    else:
+        return(r, likely)
+
+
+def correlation_Magnitude_old (strend, gtrend, shift, m):
+
+# m-1 channels up and m-1 channels down
+
+    series_strend = []
+    series_gtrend = []
+    v = 0
+    
+    for i in range(len(strend)):
+        
+         
+        v = (abs(strend[i]) // (1/m)) + 1
+        
+        if strend[i] >= 0 :
+            if strend[i] == 0 : 
+                series_strend.append(0)
+            else:
+                series_strend.append(v)
+        else:
+            series_strend.append(-v)
+    
+    
+    for i in range(len(gtrend)):
+        
+        v = (abs(gtrend[i]) // (1/m)) + 1
+        if v > m : v = m
+        
+        if gtrend[i] >= 0 :
+            if gtrend[i] == 0 : 
+                series_gtrend.append(0)
+            else:
+                series_gtrend.append(v)
+        else:
+            series_gtrend.append(-v)    
+    
+    print ('strend')
+    print (strend)
+    print ('gtrend')
+    print (gtrend)
+    print ('series_s')
+    print (series_strend)
+    print ('series_g')
+    print (series_gtrend)
+    
+    count = 0 
+    a = 0
+    alpha = 1  # tradeoff tra magnitude e direzione del trend
+    
+    for i in range(min(len(series_strend), len(series_gtrend)) - shift):
+        a = np.sign(series_strend[i+shift] * series_gtrend[i])
+        a *= np.exp(-alpha* abs( abs(series_strend[i + shift]) - abs(series_gtrend[i]) ) )
+        count += a
+   
+    return (count)
+
+def correlation_Magnitude (strend, gtrend, shift):
+ 
+    series_strend = []
+    series_gtrend = []
+ 
+ 
+    for i in range(len(strend)-1):
+ 
+        if (strend[i] != 0):
+            inc = (strend[i+1] - strend[i])/strend[i]   
+        else:
+            inc = strend[i+1] - strend[i]  # come se facesse /1 cioè l' incremento relativo risetto al max (DA CAMBIARE)
+        series_strend.append(inc)
+ 
+ 
+    for i in range(len(gtrend)-1):
+        if (gtrend[i] != 0):
+            inc = (gtrend[i+1] - gtrend[i])/gtrend[i]    
+        else:
+            inc = gtrend[i+1] - gtrend[i]  # come se facesse /1 cioè l' incremento relativo risetto al max (DA CAMBIARE)
+        series_gtrend.append(inc)
+ 
+    ## Testing/debugging    
+
+    #    print ('strend')
+    #    print (strend)
+    #    print ('gtrend')
+    #    print (gtrend)
+    #    print ('series_s')
+    #    print (series_strend)
+    #    print ('series_g')
+    #    print (series_gtrend)
+ 
+    count = 0 
+    a = 0
+    alpha = 1  # tradeoff tra magnitude e direzione del trend
+ 
+    for i in range(min(len(series_strend), len(series_gtrend)) - shift):
+        a = np.sign(series_strend[i+shift] * series_gtrend[i])
+        a *= math.exp(-alpha* abs( abs(series_strend[i + shift]) - abs(series_gtrend[i]) ) )
+        count += a
+ 
+    return count/(min(len(series_strend), len(series_gtrend)) - shift)
+
+
+def compute_dates_vec(trend_dates):
+    dates = []
+    sw = 0
+    prev = -1
+    for d in trend_dates:
+        if type(d) == str:
+            d = datetime.strptime(d,'%Y-%m-%d').timetuple().tm_yday
+            # leap years
+            if int(d[:4]) %4 == 0:
+                l = 1
+            else:
+                l = 0 
+        else:
+            l = int(d.is_leap_year)
+            d = d.dayofyear
+            # d = str(d).split()[0]
+            # d = datetime.strptime(d,'%Y-%m-%d').timetuple().tm_yday
+        if prev > d:
+            sw += 365 + l
+        # elif prev == 0:
+        #     sw -= d-1
+        prev = d
+        dates.append(d+sw) 
+    return dates
+
+def shifted_coorelation(trend, predictor_trend, max_shift=20, return_std=False, corr_type="classic"):
+    dim = min(max_shift, len(trend))
+    corr_vec = []
+    lk_vec = []
+    std_vec = []
+
+    if corr_type == "classic":
+        for shift in range(dim):
+            if return_std:
+                corr, lk, std = correlation(trend, predictor_trend, shift, return_std=return_std)
+                std_vec.append(std)
+            else:
+                corr, lk = correlation(trend, predictor_trend, shift, return_std=return_std)
+            corr_vec.append(corr)
+            lk_vec.append(lk)
+
+        if return_std:
+            return corr_vec, lk_vec, std_vec
+        else:
+            return corr_vec, lk_vec
+    elif corr_type == "magnitude":
+        for shift in range(dim):
+            corr = correlation_Magnitude(trend, predictor_trend, shift)
+            corr_vec.append(corr)
+        return corr_vec, None
+    else:
+        raiseExceptions("Unknown correlation fucntion")
+
+
+def maxmin_shift(trend, predictor_trend, max_shift=20, return_lk=False, corr_type="classic"):
+    corr_vec, lk_vec = shifted_coorelation(trend, predictor_trend, max_shift=max_shift, corr_type=corr_type)
+    min_shift = np.argmin(corr_vec)
+    min_c = corr_vec[min_shift]
+    max_shift = np.argmax(corr_vec)
+    max_c = corr_vec[max_shift]
+    if return_lk:
+        return max_shift, max_c, lk_vec[max_shift], min_shift, min_c, lk_vec[min_shift]
+    else:
+        return max_shift, max_c, min_shift, min_c
+
+def conf_int_prob(z):
+    return 1 - (1.0 + math.erf(z / math.sqrt(2.0)))/2.0
